d9c16b8878f42c6092a27819232c118ce0bade54
Wililee <leewks@gmail.com>
2022-01-15 16:58:46 -0500

    removed another thing

--- js/server.js
+++ js/server.js
@@ -11,7 +11,7 @@ app.get('/', async (req, res) => {
   try {
     const response = await axios.post('https://api.assemblyai.com/v2/realtime/token', // use account token to get a temp user token
       { expires_in: 3600 }, // can set a TTL timer in seconds.
-      { headers: { authorization: '31b44b1c29114795b06b7a276ca486e1' } }); // AssemblyAI API Key goes here
+      { headers: { authorization: '' } }); // AssemblyAI API Key goes here
     const { data } = response;
     res.json(data);
   } catch (error) {
